1f5987612ec50978c6e017e72c87315df914cdf4
fishiversity <41026299+fishiversity@users.noreply.github.com>
2023-04-26 10:58:57 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    activity = receivedNumber
+})
 function armUp () {
     wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 60)
+}
+function stop () {
+    wuKong.stopAllMotor()
 }
 function reverse () {
     wuKong.setAllMotor(-20, -20)
@@ -10,9 +16,33 @@ function armDown () {
 function forward () {
     wuKong.setAllMotor(20, 20)
 }
+let activity = 0
 joystickbit.initJoystickBit()
 radio.setGroup(30)
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-	
+    if (activity == 1) {
+        forward()
+    } else if (activity == 2) {
+        reverse()
+    } else if (activity == 5) {
+        armUp()
+    } else if (activity == 6) {
+        armDown()
+    } else if (activity == 0) {
+        stop()
+    }
+})
+basic.forever(function () {
+    if (joystickbit.getRockerValue(joystickbit.rockerType.Y) > 800) {
+        radio.sendNumber(1)
+    } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) < 200) {
+        radio.sendNumber(2)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
+        radio.sendNumber(5)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
+        radio.sendNumber(6)
+    } else {
+        radio.sendNumber(0)
+    }
 })
